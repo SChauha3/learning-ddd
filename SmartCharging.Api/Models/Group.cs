d3ced61de3c22e36af891dc35da8f94fc1598d88
indu <sachin.chauhan@telenetgroup.be>
2025-05-25 11:17:30 +0200

    Created domain models

---
+++ SmartCharging.Api/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.Api.Models
+{
+    public class Group
+    {
+        [Key]
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public int Capacity { get; private set; }
+        public ICollection<ChargeStation> ChargeStations { get; private set; } = new List<ChargeStation>();
+    }
+}
